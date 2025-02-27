b0c68710ea24428e62a95de33690740f725e7608
Khagendra Tiwari <162550347+KhagendraTiwari@users.noreply.github.com>
2025-02-27 12:39:27 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -65,31 +65,6 @@ resource "aws_nat_gateway" "nat" {
     Name = "NAT-Gateway"
   }
 }
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.main_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
-
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_route_table.id
-}
-
-resource "aws_eip" "nat_eip" {
-  domain = "vpc"
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet.id
-  tags = {
-    Name = "NAT-Gateway"
-  }
-}
 resource "aws_security_group" "instance_sg" {
   vpc_id = aws_vpc.main_vpc.id
 
